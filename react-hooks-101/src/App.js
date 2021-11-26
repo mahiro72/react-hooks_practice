8d3dc821dfbbda26a688fdaee76024b471175cf2
mahiro <mahiro.yoshie@gmail.com>
2021-11-27 02:01:58 +0900

    lesson 6

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 
 const App = ()=>{
+  const [count,setCount] = useState(1);
+
+  const calc = ()=>{
+    setCount((pre)=>{
+      return pre*2
+    })
+  }
+
+  const mod_three = ()=>{
+    setCount((pre)=>{
+      return (pre%3===0 ? pre/3 : pre)
+    })
+  }
+  
   return (
     <div>
-      <p>hello</p>
+      <p>count:{count}</p>
+      <button onClick={()=>setCount(count+1)}>+1</button>
+      <button onClick={()=>setCount(count-1)}>-1</button>
+      <button onClick={calc}>*2</button>
+      <button onClick={()=>setCount(0)}>reset</button>
+      <button onClick={mod_three}>3の倍数の時だけ3で割る</button>
     </div>
   );
 }
